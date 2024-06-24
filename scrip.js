7a7de9e768f27ec42374a893555b73557355c14d
JheanCarlo <jheanquintana905@gmail.com>
2024-06-24 17:55:48 -0500

    asdasdas

--- scrip.js
+++ scrip.js
@@ -4,11 +4,11 @@ const elementos1 = document.getElementById('lista-1');
 const lista = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
-cargarEventListeners() {
+cargarEventListeners() 
   elementos1.addEventListener('click', comprarElemento);
   carrito.addEventListener('click', eliminarElemento);
   vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-}
+
 
 function comprarElemento(e) {
   e.preventdefault();
